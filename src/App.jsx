269c5ceb6b9e8d5b31a20344887b2753f83b09b0
Lívia Calderan <liv_calderan@hotmail.com>
2026-03-04 16:28:10 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <h1 class="flex items-center justify-center h-screen bg-gray-800 text-white font-bold text-2xl">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
